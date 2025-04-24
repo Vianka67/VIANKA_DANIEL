731b4d07e5b22e9fd32b00669e86d24a4b486c34
danilohm1988 <danilohm1988320@gmail.com>
2025-04-23 20:24:45 -0400

    Agregar clases Nodo y Cola con método enqueue para gestionar una cola

---
+++ clase_nodo_cola/practica1.py
@@ -0,0 +1,20 @@
+class Nodo:
+    def __init__(self, valor):
+        self.valor = valor  # El valor que almacena el nodo
+        self.siguiente = None  # Apunta al siguiente nodo en la cola
+
+class Cola:
+    def __init__(self):
+        self.frente = None  # El primer nodo (frente de la cola)
+        self.final = None  # El último nodo (final de la cola)
+        self.tamaño = 0  # El tamaño de la cola
+
+    # Encolar (agregar un elemento al final de la cola)
+    def enqueue(self, valor):
+        nuevo_nodo = Nodo(valor)
+        if self.final is None:
+            self.frente = self.final = nuevo_nodo  # Si la cola está vacía
+        else:
+            self.final.siguiente = nuevo_nodo  # Conectamos el nodo al final
+            self.final = nuevo_nodo  # Ahora el nuevo nodo es el final de la cola
+        self.tamaño += 1
